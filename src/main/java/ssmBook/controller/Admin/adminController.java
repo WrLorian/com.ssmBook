34c91edd22415ffbc25841353c54f83d15c1d2dd
Gimmick <gbwjq2010@163.com>
2020-07-13 04:07:33 +0800

    更新了后台控制层中的admin部分以及book部分

--- src/main/java/ssmBook/controller/Admin/adminController.java
+++ src/main/java/ssmBook/controller/Admin/adminController.java
@@ -3,10 +3,16 @@ package ssmBook.controller.Admin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import ssmBook.pojo.admin;
 import ssmBook.service.AdminService;
+import ssmBook.util.PageUtil;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Objects;
 
 public class adminController {
 
+    private static final int size = 10;
     /**
      *原先项目中的该变量设置为自动填装@Autowired在service层中自动实现
      *考虑到貌似大家都对spring不太熟悉，则也可通过构造函数的方式实现^^
@@ -18,39 +24,98 @@ public class adminController {
 
 
     /**
+     * 判断用户名是否存在
+     */
+
+    /**
+     * 管理员登录
+     */
+    @RequestMapping("/login")
+    public String login(admin admin, HttpServletRequest request)
+    {
+        if(adminService.checkUser(admin.getAdminName(),admin.getPassWord()))
+        {
+            request.getSession().setAttribute("admin",admin.getAdminName());
+            return "admin/main";
+        }
+        request.setAttribute("msg","用户密码错误");
+        return "admin/login";
+    }
+
+    /**
+     * 管理员注销
+     */
+    @RequestMapping("/loggout")
+    public String logout(HttpServletRequest request)
+    {
+        request.getSession().removeAttribute("admin");
+        return "admin/login";
+    }
+    /**
      * 管理员列表
      * @return
      */
     @RequestMapping("/adminList")
-    public String adminList()
+    public String adminList(HttpServletRequest request,
+                            @RequestParam(required = false,defaultValue = "1")int page)
     {
-        return "0";
+        request.setAttribute("adminList",adminService.getAdminList(page,size));
+        request.setAttribute("pageTool", PageUtil.getPageToolAdmin(request,adminService.getTotal(),page,size));
+
+        return "admin/admin-list";
     }
 
     /**
-     * 添加管理员
+     * 跳转添加管理员页面
+     */
+    @RequestMapping("/adminAd")
+    public String adminAd()
+    {
+//        返回到添加管理员页面
+        return "admin/admin-add";
+    }
+
+    /**
+     * 添加管理员页面的添加操作
      */
     @RequestMapping("/adminAdd")
-    public String adminAdd()
+    public String adminAdd(HttpServletRequest request,admin admin)
     {
-        return "0";
+        if(adminService.isExist(admin.getAdminName()))
+        {
+            request.setAttribute("msg","用户名已存在！");
+            return "admin/admin-add";
+        }
+        adminService.adminAdd(admin);
+        return "redirect:adminList";
+    }
+    /**
+     * 跳转重置管理员密码页面（需获取需要更改的管理员的ID）
+     */
+    @RequestMapping("/adminRe")
+    public String adminResetPwd(HttpServletRequest request,int adminId)
+    {
+        request.setAttribute("admin",adminService.selectAdminById(adminId));
+        return "admin/admin";
     }
 
     /**
      * 重置管理员密码
      */
     @RequestMapping("/adminResetPwd")
-    public String adminResetPwd()
+    public String adminResetPwd(admin admin)
     {
-        return "0";
+        adminService.updateAdmin(admin);
+        return "redirect:adminList";
     }
 
     /**
      * 删除管理员
      */
     @RequestMapping("/adminDelect")
-    public String adminDelect()
+    public String adminDelect(int adminId)
     {
-        return "";
+        adminService.deleteAdmin(adminId);
+        return "redirect:adminList";
     }
 }
--- src/main/java/ssmBook/controller/Admin/bookController.java
+++ src/main/java/ssmBook/controller/Admin/bookController.java
@@ -2,10 +2,18 @@ package ssmBook.controller.Admin;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import ssmBook.pojo.book;
 import ssmBook.service.BookService;
+import ssmBook.service.CategoryService;
+import ssmBook.util.PageUtil;
+
+import javax.servlet.http.HttpServletRequest;
+
 
 public class bookController {
 
+    private static final int size = 10;
     /**
      *原先项目中的该变量设置为自动填装@Autowired在service层中自动实现
      *考虑到貌似大家都对spring不太熟悉，则也可通过构造函数的方式实现^^
@@ -14,6 +22,8 @@ public class bookController {
      */
     @Autowired
     private BookService bookService;
+    @Autowired
+    private CategoryService categoryService;
 
     /**
      * 图书列表
@@ -21,32 +31,79 @@ public class bookController {
      * 如有困难，可将该方法分成3个方法，逐一编写
      */
     @RequestMapping("/bookList")
-    public String bookList()
+    public String bookList(HttpServletRequest request,
+        @RequestParam(required=false, defaultValue="0")int status,
+        @RequestParam(required=false, defaultValue="1")int page)
     {
-        return "0";
+        if(status==0)
+        {
+            request.setAttribute("bookList",bookService.getBookList(page,size));
+            request.setAttribute("pageTool", PageUtil.getPageToolAdmin(request,bookService.getTotal(),page,size));
+        }
+        else if(status == 1) {
+            request.setAttribute("bookList", bookService.selectListIsSpecial(page, size));
+            request.setAttribute("pageTool", PageUtil.getPageToolAdmin(request, bookService.getTotalIsSpecial(), page, size));
+        }else if (status == 2) {
+            request.setAttribute("bookList", bookService.getListIsNews(page, size));
+            request.setAttribute("pageTool", PageUtil.getPageToolAdmin(request, bookService.getTotalIsNews(), page, size));
+        }
+        return "/admin/book-list";
+    }
+
+    /**
+     * 跳转到图书添加页面
+     */
+    @RequestMapping("/bookAd")
+    public String bookAd(HttpServletRequest request)
+    {
+        request.setAttribute("categoryList",categoryService.getCategoryList());
+        return "admin/book-add";
     }
 
     /**
      * 图书添加
      */
     @RequestMapping("/bookAdd")
-    public String bookAdd()
+    public String bookAdd(book book)
     {
-        return "0";
+        bookService.bookAdd(book);
+        return "redirect:bookList";
     }
 
     /**
-     * 图书修改
+     * 跳转到图书修改
+     */
+    @RequestMapping("/bookUp")
+    public String bookUpdate(HttpServletRequest request,int bookId)
+    {
+        request.setAttribute("categoryList",categoryService.getCategoryList());
+        request.setAttribute("book",bookService.getBookById(bookId));
+        return "/admin/book-update";
+    }
+
+    /**
+     * 图书更新
      */
     @RequestMapping("/bookUpdate")
-    public String bookUpdate()
+    public String bookUpdate(book book)
     {
-        return "0";
+        bookService.bookUpdate(book);
+        return "redirect:bookList";
+    }
+
+    /**
+     * 图书删除
+     */
+    @RequestMapping("/bookDelete")
+    public String bookDelete(int id)
+    {
+        bookService.bookDelete(id);
+        return "redirect:bookList";
     }
 
     /**
      * 图书设置
-     * 设为推荐、设为最新
+     * 暂时放空。。。
      */
     @RequestMapping("/bookSet")
     public String bookSet()
