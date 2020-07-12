55937a6652b4cbfa92dc7f7b882136866daabea3
Gimmick <gbwjq2010@163.com>
2020-07-13 04:10:02 +0800

    引入的pojo层大小写修改

--- src/main/java/ssmBook/controller/Admin/indentController.java
+++ src/main/java/ssmBook/controller/Admin/indentController.java
@@ -3,7 +3,7 @@ package ssmBook.controller.Admin;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
-import ssmBook.pojo.Admin;
+import ssmBook.pojo.admin;
 import ssmBook.service.IndentService;
 
 /**
